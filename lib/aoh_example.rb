88d4192aea2d3d1cc5c08614497712cdca59d399
Blake Runyon <blakerunyon09@gmail.com>
2020-12-14 17:20:54 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,11 +18,11 @@ end
 
 def literal_aoh
   literal_aoh = [
-    DON_G = { name:  "Don Gately", occupation: "Live-in Staff" },
-    JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-    PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
-    KATE_G = { name:  "Kate Gompert", occupation: "None" },
-    BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
+    dON_G = { name:  "Don Gately", occupation: "Live-in Staff" },
+    jOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+    pAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
+    kATE_G = { name:  "Kate Gompert", occupation: "None" },
+    bRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
   ]
 end
 
